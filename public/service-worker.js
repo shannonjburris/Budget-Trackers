87f3f45b66e71efbc54e9d280e677047332209a2
Shannon <shannonjburris@gmail.com>
2021-08-20 16:48:59 -0400

    manifest update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-// fix this up
+
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -65,8 +65,6 @@ self.addEventListener("fetch", function(evt) {
     return;
   }
 
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
     caches.match(evt.request).then(function(response) {
       return response || fetch(evt.request);
